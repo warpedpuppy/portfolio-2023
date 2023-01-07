189d7871bcf027e397741956fc33d87e3d3fa4e0
ted <ted@warpedpuppy.com>
2023-01-07 11:54:09 -0500

    fixed page specific path

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,14 @@
 import './Menu.css';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 const Menu = ({ state, setState }) => {
 	let arr = ['who', 'what', 'where']
 	let navigate = useNavigate();
+	let location = useLocation();
+
+	useEffect(() => {
+		setState(location.pathname.slice(1));
+	}, [location, setState])
 
 	function goToPage(item) {
 		setState(item);
